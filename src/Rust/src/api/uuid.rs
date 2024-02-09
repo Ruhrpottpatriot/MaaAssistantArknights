547fea32dd6a4397f7e038c6c77bb9885dc6b0a5
Ruhrpottpatriot <Ruhrpottpatriot@gmx.de>
2024-02-09 11:00:18 +0100

    Refactor UUID API endpoint
    The methods for the endpoint were residing inside their own files with
    just one method each. This is an unecessary level of nesting and
    indirection. The methods have been moved to the main module file. The
    endpoint methods also have been give nproper documentation, describing
    their input and return parameters.

--- src/Rust/src/api/uuid.rs
+++ src/Rust/src/api/uuid.rs
@@ -1,7 +1,48 @@
-use actix_web::web;
-mod all;
-mod get;
+use super::Error;
+use crate::api::ManagerData;
+use crate::database;
+use actix_web::{
+    get,
+    web::{self, Path},
+    HttpResponse, Responder,
+};
+use serde_json::json;
+
+/// Registers all the `/uuid` routes
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.route("/uuid/all", web::post().to(all::all));
-    cfg.route("/uuid/get", web::post().to(get::get));
+    cfg.service(web::scope("/uuid").service(all).service(single));
+}
+
+/// Gets all UUIDs stored in the database
+///
+/// # Parameters
+/// * None
+///
+/// # Returns
+/// A list of all UUIDs stored in the database. Returns an empty array if there are no
+/// UUIDs in the database.
+#[get("")]
+async fn all() -> Result<impl Responder, Error> {
+    let uuids = database::get_all_uuid().map_err(Error::from)?;
+    Ok(HttpResponse::Ok().json(uuids))
+}
+
+/// Gets the UUID of a specific MAA instance
+///
+/// # Parameters
+/// * `id` - The ID of the MAA instance
+///
+/// # Returns
+/// The UUID of the MAA instance, or a 404 not found error if the instance does not exist
+#[get("/{id:\\d+}")]
+pub async fn single(id: Path<i64>, manager: ManagerData) -> Result<impl Responder, Error> {
+    let mut manager = manager.lock().map_err(Error::from)?;
+    let id = id.into_inner();
+
+    let maa = manager.get_mut(id).ok_or(Error::InstanceNotFound)?;
+    let uuid = maa.get_uuid()?;
+
+    Ok(HttpResponse::Ok().json(json!({
+        "uuid": uuid
+    })))
 }
