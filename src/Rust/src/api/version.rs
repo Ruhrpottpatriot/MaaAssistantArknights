5a3d6bdfd92c0b17c963dc8f05af6df995af8d0e
Ruhrpottpatriot <Ruhrpottpatriot@gmx.de>
2024-02-09 11:00:18 +0100

    Refactor the version endpoint
    Removes the result field from the version response. Success or failure
    should always be indicate by HTTP status codes, or an 200 OK response
    with a proper error message in the body.

--- src/Rust/src/api/version.rs
+++ src/Rust/src/api/version.rs
@@ -3,15 +3,28 @@ use super::Error;
 use actix_web::{HttpResponse, Responder, web};
 use serde_json::json;
 
+/// Registers the `/version` route
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.route("/version", web::get().to(version));
+}
+
+/// Gets the version of the MAA core library and the server
+/// 
+/// # Parameters
+/// * None
+/// 
+/// # Returns
+/// A JSON object containing the version of the MAA core library and the server, e.g.
+/// ```json
+/// {
+///     "core": "v0.0.1",
+///     "server": "v0.1.0"
+/// } 
+/// ```
 pub async fn version() -> Result<impl Responder, Error> {
     let core_version = Maa::get_version()?;
     Ok(HttpResponse::Ok().json(json!({
         "core":core_version,
-        "server":SERVER_VERSION,
-        "result":  0,
+        "server":SERVER_VERSION
     })))
-}
-
-pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.route("/version", web::get().to(version));
 }
