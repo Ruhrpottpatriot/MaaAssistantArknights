25ffb7a2c83cecd515064aff233a21a05b9e2279
Ruhrpottpatriot <Ruhrpottpatriot@gmx.de>
2024-02-09 11:00:18 +0100

    Bump server version

--- src/Rust/src/main.rs
+++ src/Rust/src/main.rs
@@ -10,16 +10,22 @@ use std::sync::Mutex;
 use maa_sys::Maa;
 use actix_web::{middleware, rt, App, HttpServer, web};
 
-const SERVER_VERSION:&str="v0.0.1";
+
+const SERVER_VERSION:&str="v0.1.0";
+
 fn main() -> std::io::Result<()> {
     Maa::load_resource(&CONFIG.resource.path).unwrap();
+
     if CONFIG.database.drop_on_start_up{
         database::drop_all().unwrap();
     }
+
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         .init();
+
     let maa_manager = web::Data::new(Mutex::new(api::MaaManager::new()));
+
     rt::System::new().block_on(async {
         HttpServer::new(move|| {
             App::new()
