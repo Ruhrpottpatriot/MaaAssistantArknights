417df4c336981e7adbf646037d775d26ab2263fd
Ruhrpottpatriot <Ruhrpottpatriot@gmx.de>
2024-02-09 11:00:17 +0100

    Remove spurious serde rename
    Serde will (de-)serialize fields snake_case. In case of the current
    documentation, the fields would've been (de-)serialized to the same name
    as the #[serde(rename = "...")] attribute set them to.

--- src/Rust/src/config.rs
+++ src/Rust/src/config.rs
@@ -18,36 +18,28 @@ lazy_static! {
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
-    #[serde(rename = "server")]
     pub server: Server,
 
-    #[serde(rename = "database")]
     pub database: Database,
 
-    #[serde(rename = "resource")]
     pub resource: Resource,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Database {
-    #[serde(rename = "path")]
     pub path: String,
 
-    #[serde(rename = "drop_on_start_up")]
     pub drop_on_start_up: bool,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Server {
-    #[serde(rename = "address")]
     pub address: String,
 
-    #[serde(rename = "port")]
     pub port: u16,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Resource {
-    #[serde(rename = "path")]
     pub path: String,
 }
